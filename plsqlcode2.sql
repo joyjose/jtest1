b31e000bc3cf5b2f63e93f5dbf527d50cd4f783c
Joy Jose <joy.jose@oracle.com>
2017-07-20 00:09:44 -0700

    jt3 ph

--- plsqlcode2.sql
+++ plsqlcode2.sql
@@ -1,7 +1,7 @@
 code line 1 r12 new changes
 code line 2
 //ph changes
-code line 3 jt1 master jt2
+code line 3 jt1 master jt2 jt3ph 
 code line 4 master changes
 //r12 retrofit changes done here
 code line 5 jt1 r12
